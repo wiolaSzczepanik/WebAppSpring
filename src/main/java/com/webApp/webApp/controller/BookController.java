402b0a21a55343702b59288acd2cefde20c297be
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-09-01 11:45:37 +0200

    implement method in getBooks in BookController

--- src/main/java/com/webApp/webApp/controller/BookController.java
+++ src/main/java/com/webApp/webApp/controller/BookController.java
@@ -1,7 +1,22 @@
 package com.webApp.webApp.controller;
 
+import com.webApp.webApp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){ // Model interface in Spring, spring will be passing Model obj in this method
+        model.addAttribute("books", bookRepository.findAll());
+        return "books"; // return the view name
+    }
 }
